f85c8fa9452018ce2ba4cb38b189019c87193358
Hovhannes Sargsyan <sarhov@sarhov.com>
2013-11-14 19:51:39 +0400

    update - i menu's width > 750

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,14 @@
 jQuery(document).ready(function($) {
+	var topMenuItem = $('.main-nav > .main-nav-item');
+	if (topMenuItem.length > 3 && $('.main-nav').width() > 750)
+		{
+			topMenuItem.last().addClass('lastItem');
+			alert('gogo');
+
+}
+
+
+
+
 	
 });
